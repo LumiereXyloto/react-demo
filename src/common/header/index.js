204624649add58351e3085048daee8c7bbc69479
Lumiere Xyloto <740207968@qq.com>
2019-05-05 20:19:24 +0800

    feat: 完成header组件大致开发

---
+++ src/common/header/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store';
+import {
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button,
+  SearchWrapper
+} from './style'
+
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载APP</NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <NavItem className='right'>
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.focused}
+            timeout={300}
+            classNames="slide"
+          >
+            <NavSearch
+              className={props.focused ? 'focused' : ''}
+              onFocus = {props.handleInputFocus}
+              onBlur = {props.handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='writing'>
+          <i className="iconfont">&#xe615;</i>
+          写文章
+        </Button>
+        <Button className='reg'>注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    focused: state.get('header').get('focused')
+    // 等价 focused: state.getIn(['header', 'focused'])
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      const action = actionCreators.searchFocus()
+      dispatch(action)
+    },
+  
+    handleInputBlur() {
+      const action = actionCreators.searchBlur()
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ src/common/header/store/constants.js
@@ -0,0 +1,2 @@
+export const SEARCH_FOCUS = 'headr/SEARCH_FOCUS';
+export const SEARCH_BLUR = 'header/SEARCH_BLUR';
